8facfae23573b86c5e11a3a7035284f1948f1b71
bersa-tech <saberllit@gmail.com>
2024-11-16 02:59:07 +0100

    correction du slider (key, index, checked, tri)

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,25 +8,28 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
   );
   const nextCard = () => {
-    setTimeout(() => setIndex(index < byDateDesc.length ? index + 1 : 0), 5000);
+    setTimeout(
+      () => setIndex(index + 1 < byDateDesc?.length ? index + 1 : 0),
+      5000
+    );
   };
   useEffect(() => {
     nextCard();
   });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div key={event.date}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={event.cover} alt={event.title} />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
                 <h3>{event.title}</h3>
@@ -39,18 +42,27 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={_.date}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
+                  readOnly
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
 };
 
 export default Slider;
+
+// 1- modifier l'operateur de tri
+// 2- modifier l'index avec le calcul +1 et ? existance de byDateDesc
+// 3- modifier et isoler le key <div key={event.date}>
+// 4- modifier le alt avec alt={event.title}
+// 5- modifier le key de la radio avec key={_.date}
+// 6- modifier le checked={idx === radioIdx} vers checked={index === radioIdx}
+// 7- retrait des <> </> car cela crée une erreur sur les key en reliant les blocs
